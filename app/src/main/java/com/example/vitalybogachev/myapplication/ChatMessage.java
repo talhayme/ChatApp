d9cccab493bd83955043b0d4e6928826c4e53b8f
testone <bogachev.vitaliy91test@gmail.com>
2017-07-08 17:18:58 +0300

    Chat app
    Chat

---
+++ app/src/main/java/com/example/vitalybogachev/myapplication/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.example.vitalybogachev.myapplication;
+
+/**
+ * Created by vitalybogachev on 08.07.17.
+ */
+
+public class ChatMessage {
+
+
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left , String message) {
+
+
+        super();
+        this.left=left;
+        this.message = message;
+
+    }
+
+}
